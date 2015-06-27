7ddac61317ed6bc1449dca54f5baafc98b5be6d3
xianfa <kferrari430@gmail.com>
2015-06-28 05:15:56 +0800

    2

--- app/src/main/java/com/orange/maiboon/List.java
+++ app/src/main/java/com/orange/maiboon/List.java
@@ -1,39 +1,24 @@
 package com.orange.maiboon;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity {
-
-    Button btn;
+public class List extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), List.class);
-                startActivity(i);
-            }
-        });
+        setContentView(R.layout.activity_list);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_list, menu);
         return true;
     }
 

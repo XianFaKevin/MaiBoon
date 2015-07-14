2d04faa55a3bcfcdda8eb3847661b1fc00ea8974
xianfa <kferrari430@gmail.com>
2015-07-15 01:32:54 +0800

    final

--- app/src/main/java/com/orange/maiboon/MainActivity.java
+++ app/src/main/java/com/orange/maiboon/MainActivity.java
@@ -1,6 +1,8 @@
 package com.orange.maiboon;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +11,19 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapCircleThumbnail;
+
+import java.util.Random;
 
 
 public class MainActivity extends Activity {
 
     BootstrapButton acBtn;
-    Button seedBtn, settingsBtn;
+    Button settingsBtn;
     BootstrapButton addAcBtn;
     BootstrapButton logBtn;
     BootstrapButton addLogBtn;
+    BootstrapCircleThumbnail bb;
     SQLiteHelper dbHelper = new SQLiteHelper(this);
 
     @Override
@@ -64,10 +70,52 @@ public class MainActivity extends Activity {
         settingsBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                dbHelper.reset();
-                Toast.makeText(getApplicationContext(), "Reset!", Toast.LENGTH_LONG);
+                settingsDialog(MainActivity.this);
             }
         });
+
+        bb = (BootstrapCircleThumbnail) findViewById(R.id.bb);
+        setImage();
+    }
+
+    public void settingsDialog(final Activity context) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context, 2);
+        // set dialog message
+        alertDialogBuilder
+                .setTitle("确定清零资料库？")
+                //.setMessage("确定清零资料库？")
+                .setNegativeButton("否", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setIcon(R.drawable.warning)
+                .setPositiveButton("是", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dbHelper.reset();
+                        Toast.makeText(getApplicationContext(), "资料库清零完毕", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    public void setImage() {
+        int count = new Random().nextInt(9);
+        if (count == 0) bb.setImage(R.drawable.a1);
+        else if (count == 1) bb.setImage(R.drawable.a2);
+        else if (count == 2) bb.setImage(R.drawable.a4);
+        else if (count == 3) bb.setImage(R.drawable.a5);
+        else if (count == 4)bb.setImage(R.drawable.a6);
+        else if (count == 5)bb.setImage(R.drawable.a7);
+        else if (count == 6) bb.setImage(R.drawable.a8);
+        else if (count == 7)bb.setImage(R.drawable.a10);
+        else if (count == 8)bb.setImage(R.drawable.a12);
     }
 
 }
